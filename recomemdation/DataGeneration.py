cfd92a14795c4976b659193d34af9d48df49b6b6
AFTG\xiaofeng.li <xiaofeng.li@afusion.com>
2018-05-27 16:34:43 +0800

    spark code

--- recomemdation/DataGeneration.py
+++ recomemdation/DataGeneration.py
@@ -1,19 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import findspark
+findspark.init()
+import pyspark
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+spark=SparkSession.builder.getOrCreate()
+sc=spark.sparkContext
 
-data=pd.read_csv('C:/Users/xiaofeng.li.AFTG/Downloads/AI-Data.csv')
-#print(data.head())
+df=pd.read_csv('C:/Users/xiaofeng.li.AFTG/Google Drive/Data/AI-Data.csv')
+df=df[['PlayerID','GameID','BetAmount']]
+df_spark=spark.createDataFrame(df)
+#df_spark.show(20)
 
-data=data[['GameID','PlayerID','BetAmount']]
-user_sum=data.groupby(['PlayerID'])['BetAmount'].sum()
-data_group=data.groupby(['PlayerID','GameID'])['BetAmount'].sum()
-rating=pd.DataFrame(index=range(0,data_group.size),columns=['userID','subgameID','rating'])
+user_sum=df_spark.groupby('PlayerID').sum('BetAmount')
+user_sum=user_sum.select(F.col('PlayerID').alias("PlayerID"),F.col('sum(BetAmount)').alias("totalAmt"))
 
-for i in range(0,len(rating.index)):
-    rating.iloc[i,0]=data_group.index[i][0]
-    rating.iloc[i,1]=data_group.index[i][1]
-    rating.iloc[i,2]=(data_group.iloc[i]/user_sum.get(data_group.index[i][0]))*10
-print(rating.head())
-rating.to_csv('rating.csv')
+df_group=df_spark.groupby('PlayerID','GameID').sum('BetAmount')
+df_group=df_group.select(F.col('PlayerID').alias('PlayerID'),F.col('GameID').alias('GameID'),F.col('sum(BetAmount)').alias('amt'))
 
+data=df_group.join(user_sum,on=['PlayerID'],how='left_outer')
+#data.show(20)
 
+data=data.withColumn('rate',(F.col("amt"))/(F.col("totalAmt"))*10).drop("amt","totalAmt")
+#data.show(20)
+
+rating=data.toPandas()
--- recomemdation/DataPreperation.py
+++ recomemdation/DataPreperation.py
@@ -1,11 +1,8 @@
-#from DataGeneration import *
+from DataGeneration import *
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-rating=pd.read_csv('C:/Users/xiaofeng.li.AFTG/Google Drive/Data/rating.csv')
-rating=rating[['PlayerID','GameID','rate']]
-print('rating.csv reading done.')
 print(rating.columns)
 MNS=MinMaxScaler()
 rating_scaler=MNS.fit_transform(rating['rate'].values.reshape(-1,1))
